3a44f0f4612972c76bffd671346b518e2e3eef0b
Vlarumal <177475520+Vlarumal@users.noreply.github.com>
2025-04-23 18:26:57 +0300

    fix: url

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,1 +1,2 @@
-export const apiBaseUrl = 'http://localhost:3001/api';
+// export const apiBaseUrl = 'http://localhost:3001/api';
+export const apiBaseUrl = 'https://patientapp-wzu7.onrender.com/api';
